13f346c7f58012b61408d14869ed398fbd17e769
Pavel Yatskevich <yatskevich199902@gmail.com>
2024-12-09 23:54:16 +0300

    fixed ReviewRepositoryTest

--- moderation/src/test/java/com/yatskevich/hs/spring/moderation/repository/ReviewRepositoryTest.java
+++ moderation/src/test/java/com/yatskevich/hs/spring/moderation/repository/ReviewRepositoryTest.java
@@ -11,13 +11,15 @@ import java.util.UUID;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.Rollback;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.jdbc.Sql;
 import org.springframework.transaction.annotation.Transactional;
 
+@ActiveProfiles({"test"})
 @SpringBootTest
 @Transactional
-@ActiveProfiles({"test"})
+@Rollback
 class ReviewRepositoryTest extends AbstractTestcontainersTests {
 
     @Autowired
