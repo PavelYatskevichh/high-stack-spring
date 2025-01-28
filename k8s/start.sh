20966fd0650cd9bb97cf3047bb8fb9dad5514d30
Pavel Yatskevich <pavel_yatskevich@senla.eu>
2025-01-28 17:16:15 +0300

    changed start.sh

--- k8s/start.sh
+++ k8s/start.sh
@@ -11,6 +11,12 @@ fi
 
 . k8s/build-images.sh
 
+echo "Checking for installed ingress-controller ..."
+if [[ $ingress_flag == true && -z $(kubectl get pod -l app.kubernetes.io/name=ingress-nginx --all-namespaces) ]]; then
+  echo "Installing ingress-controller ..."
+  minikube addons enable ingress
+fi
+
 echo "Checking for installed cloudnative-pg operator ..."
 if [[ $cnpg_flag == true && -z $(kubectl get pod -l app.kubernetes.io/name=cloudnative-pg --all-namespaces) ]]; then
   echo "Installing cloudnative-pg operator ..."
@@ -23,12 +29,6 @@ if [[ $strimzi_flag == true && -z $(kubectl get pod -l name=strimzi-cluster-oper
   kubectl create namespace "$STRIMZI_NAMESPACE"
   echo "Installing strimzi-cluster-operator ..."
   kubectl create -f "https://strimzi.io/install/latest?namespace=$STRIMZI_NAMESPACE" -n "$STRIMZI_NAMESPACE"
-fi
-
-echo "Checking for installed ingress-controller ..."
-if [[ $ingress_flag == true && -z $(kubectl get pod -l app.kubernetes.io/name=ingress-nginx --all-namespaces) ]]; then
-  echo "Installing ingress-controller ..."
-  minikube addons enable ingress
 fi
 
 kubectl wait pod --all --for=condition=ready --timeout="$TIMEOUT" -l app.kubernetes.io/name=cloudnative-pg --all-namespaces \
